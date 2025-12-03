2e6e26a928e62541c9c70405a7dc5764db11aae7
Somadina <kingswatter@gmail.com>
2025-12-03 11:51:14 +0100

    updated some components and created a toast system for proper feedback.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,23 +3,26 @@ import './globals.css';
 
 import { SidebarProvider } from '@/context/SidebarContext';
 import { ThemeProvider } from '@/context/ThemeContext';
+import { ToastProvider } from '@/context/ToastContext';
 
 const outfit = Outfit({
-  subsets: ["latin"],
+	subsets: ['latin'],
 });
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body className={`${outfit.className} dark:bg-gray-900`}>
-        <ThemeProvider>
-          <SidebarProvider>{children}</SidebarProvider>
-        </ThemeProvider>
-      </body>
-    </html>
-  );
+	return (
+		<html lang="en">
+			<body className={`${outfit.className} dark:bg-gray-900`}>
+				<ThemeProvider>
+					<ToastProvider>
+						<SidebarProvider>{children}</SidebarProvider>
+					</ToastProvider>
+				</ThemeProvider>
+			</body>
+		</html>
+	);
 }
